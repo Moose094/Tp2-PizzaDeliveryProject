cbc2d3c8fe2158de5386ca19258aed2847f2bd30
Moose094 <38635072+Moose094@users.noreply.github.com>
2020-04-12 12:09:15 -0500

    Update Topping.java

--- PizzaProject/src/Topping.java
+++ PizzaProject/src/Topping.java
@@ -3,6 +3,13 @@ public abstract class Topping {
 	
 	String name;
 	double price;
+	
+	// constructor for the topping class 
+	public Topping(String name, double price) {
+		
+		this.name = name;
+		this.price = price;
+	}
 
 	public double getPrice() {
 		return price;
