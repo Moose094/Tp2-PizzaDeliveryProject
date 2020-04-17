5dcaba0fc6fe0d6efac0636978297b31a03ad852
Kaleb Olson <kalebroyolson@gmail.com>
2020-04-17 15:23:33 -0500

    Adds full testing for Customer class

--- PizzaProject/junit_tests/main_test_package/TestCustomer.java
+++ PizzaProject/junit_tests/main_test_package/TestCustomer.java
@@ -11,7 +11,7 @@ class TestCustomer {
 
 	@Test
 	// testing the customer constructor
-	public void CustmonerTesting() {
+	public void CustomerTesting() {
 Customer c = new Customer ("Lionel", "Ronaldo", "2 jerk st", "Ro@email.com", "555-555");
 assertEquals("Lionel", c.getfirstName());
 assertEquals("Ronaldo", c.getlastName());
@@ -20,4 +20,43 @@ assertEquals("Ro@email.com", c.getEmailAddress());
 assertEquals("555-555", c.getphoneNumber());
 
 	}
+	
+	@Test
+	public void test_first_name() {
+		Customer c = new Customer();
+		c.setFirstName("asdef");
+		assertEquals(c.getfirstName(),"asdef");
+	}
+
+	
+	@Test
+	public void test_last_name() {
+		Customer c = new Customer();
+		c.setLastName("qwer");
+		assertEquals(c.getlastName(),"qwer");
+	}
+	
+	
+	@Test	
+	public void test_address() {
+		Customer c = new Customer();
+		c.setAddress("4545 sunshine way");
+		assertEquals(c.getAddress(),"4545 sunshine way");
+	}
+	
+	@Test	
+	public void test_email() {
+		Customer c = new Customer();
+		c.setEmailAddress("asdf@qwer.as");
+		assertTrue(c.getEmailAddress().equals("asdf@qwer.as"));
+	}
+	
+	@Test
+	public void test_phone_num() {
+		Customer c = new Customer();
+		c.setPhoneNumber("12345678901");
+		assertEquals(c.getphoneNumber(),"12345678901");
+	}
+	
 }
+
