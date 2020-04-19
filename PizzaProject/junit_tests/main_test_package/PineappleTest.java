ae8d1084fdf18ef68bd3e936e9e0dd7646412229
Moose094 <38635072+Moose094@users.noreply.github.com>
2020-04-18 19:00:44 -0500

    Create PineappleTest.java

---
+++ PizzaProject/junit_tests/main_test_package/PineappleTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PineappleTest {
+
+@Test
+	
+	public void TestPineapple() {
+		Pineapple p = new Pineapple ();
+		p.name = "pineapple";
+		p.price = .45;
+		assertEquals("pineapple", p.name);
+		assertEquals(.45, p.price);
+	}
+	
+
+}
