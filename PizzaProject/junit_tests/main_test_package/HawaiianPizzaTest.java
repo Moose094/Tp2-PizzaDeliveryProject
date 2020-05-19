86273f3f53074320d20ebeff3af76c5aa7a12bde
Moose094 <38635072+Moose094@users.noreply.github.com>
2020-05-18 19:43:34 -0500

    Create HawaiianPizzaTest.java

---
+++ PizzaProject/junit_tests/main_test_package/HawaiianPizzaTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HawaiianPizzaTest {
+	@Test
+	// test case for pizza size
+
+	public void TestSausageSize() {
+		HawaiianPizza hp = new HawaiianPizza ("");
+		hp.setPizzaSize("small");
+		assertEquals("small", hp.getPizzaSize());
+	
+	}
+
+@Test
+// test case for pizza price
+
+public void TestPepperoniPrice() {
+	HawaiianPizza hp = new HawaiianPizza ("");
+	hp.setPizzaSize("medium");
+	assertEquals(13, hp.price);
+
+}
+
+}
